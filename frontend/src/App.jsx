de8f3c8f95aa639b948087e2e5d742ee0fa253ce
Abhishek mishra <abhishekmishra992016@gmail.com>
2024-07-02 17:07:27 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Auth from "./pages/Auth/Auth";
+function App() {
+  return (
+    <>
+      <Auth />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Auth/Auth.jsx
@@ -0,0 +1,12 @@
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+const Auth = () => {
+  return (
+    <>
+      <SignIn />
+      <SignUp />
+    </>
+  );
+};
+
+export default Auth;
